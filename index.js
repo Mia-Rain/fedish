b74fee90df90acac21d1e584780914cb50714d9c
ThatGeekyWeeb <ThatGeekyWeeb@gmail.com>
2021-04-11 00:04:52 -0400

    testing time..

--- index.js
+++ index.js
@@ -15,18 +15,25 @@ function grab() {
       closeOnClickOutside: false,
     }).then((value) => {
       sessionStorage.setItem("CLID", `${value}`);
-      window.CLID = sessionStorage.getItem("CLID");
-      console.log("URL IS: " + window.URL + "\n" + "CLID IS: " + window.CLID);
-      window.URL = sessionStorage.getItem("URL");
-      window.location.replace(
-        window.URL +
-          "/oauth/authorize?client_id=" +
-          window.CLID +
-          "&scope=" +
-          scopes +
-          "&redirect_uri=urn:ietf:wg:oauth:2.0:oob" +
-          "&response_type=code"
-      );
+      swal("Client SEC", {
+        content: "input",
+        closeOnEsc: false,
+        closeOnClickOutside: false,
+      }).then((value) => {
+        sessionStorage.setItem("CLSEC", `${value}`);
+        window.CLID = sessionStorage.getItem("CLID");
+        console.log("URL IS: " + window.URL + "\n" + "CLID IS: " + window.CLID);
+        window.URL = sessionStorage.getItem("URL");
+        window.location.replace(
+          window.URL +
+            "/oauth/authorize?client_id=" +
+            window.CLID +
+            "&scope=" +
+            scopes +
+            "&redirect_uri=https://thatgeekyweeb.is-dummy-thi.cc/fedish/callback" +
+            "&response_type=code"
+        );
+      });
     });
   });
 }
